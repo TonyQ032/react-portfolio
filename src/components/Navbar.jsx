de94b76134e4262a5690496547636637c1575115
aq032 <anthonyq032@gmail.com>
2022-10-04 15:24:15 -0700

    Created basic HTML layout for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,48 @@
 import React from "react";
+import navEffects from "../utils/navbar";
+import "../styles/navbar.css";
 
 function Navbar() {
   return (
-    <>
-      <h2>Navbar</h2>
-      <p>Navbar text</p>
-    </>
+    <nav className="navbar">
+      <a href="#" className="nav-branding">
+        Anthony Quinones
+      </a>
+
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            Resume
+          </a>
+        </li>
+      </ul>
+
+      <div className="hamburger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
   );
 }
 
