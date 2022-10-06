e17056f9de0832dae18bffe4bbe66a64bb1ab2c6
aq032 <anthonyq032@gmail.com>
2022-10-05 21:05:57 -0700

    Created footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import "../styles/footer.css";
+import Resume from "../assets/AnthonyQ-Resume.pdf";
 
 function Footer() {
   return (
-    <>
-      <h2>Footer</h2>
-      <p>Footer text</p>
-    </>
+    <footer>
+      <h3>Check me out! :</h3>
+
+      <a
+        href="https://github.com/TonyQ032"
+        target="_blank"
+        className="icon-container"
+      >
+        <i class="bi bi-github"></i>
+        <p>GitHub</p>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/anthony-quinones/"
+        target="_blank"
+        className="icon-container"
+      >
+        <i class="bi bi-linkedin"></i>
+        <p>LinkedIn</p>
+      </a>
+
+      <a href={Resume} target="_blank" className="icon-container">
+        <i class="bi bi-person-circle fa-3x"></i>
+        <p>Resume</p>
+      </a>
+    </footer>
   );
 }
 
