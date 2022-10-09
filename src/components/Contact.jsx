37157a14286765ff7407d08e547a1ade593dc0c1
aq032 <anthonyq032@gmail.com>
2022-10-08 20:08:24 -0700

    Updated Contact and form css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import "../styles/about.css";
 import Swal from "sweetalert2";
 import emailjs from "@emailjs/browser";
+import "../styles/form.css";
 
 const serviceId = "default_service";
 const templateId = "template_c8segls";
@@ -45,14 +46,61 @@ export const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section className="section-container contact-main">
+      <h2 className="container-title">Contact Me!</h2>
+
+      <div className="contact-container">
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input className="form-btn" type="submit" value="Send" />
+        </form>
+
+        <div className="about-info">
+          <p>
+            <span className="bold">Name: </span>Anthony Quinones
+          </p>
+          <p>
+            <span className="bold">Title: </span>Full Stack Software Engineer
+          </p>
+          <p>
+            <span className="bold">Email: </span>anthonyq032@gmail.com
+          </p>
+          <p>
+            <span className="bold">Phone: </span>(909) 507-1224
+          </p>
+          <p>
+            <span className="bold">LinkedIn: </span>
+            <a
+              href="https://www.linkedin.com/in/anthony-quinones/"
+              target="_blank"
+              className="social-link"
+            >
+              Anthony-Quinones
+            </a>
+          </p>
+          <p>
+            <span className="bold">Github: </span>
+            <a
+              href="https://github.com/TonyQ032"
+              target="_blank"
+              className="social-link"
+            >
+              TonyQ032
+            </a>
+          </p>
+
+          <p>
+            <br />
+            If you have any questions or comments please feel free to contact
+            me! :)
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
